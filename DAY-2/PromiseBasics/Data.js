cb1c7a37c033a4bc122de304f748a4befda31cab
Mr. Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-31 16:49:11 +0530

    Delete DAY-2 directory

--- DAY-2/PromiseBasics/Data.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`Hello ${name}`);
-      //   reject("Error !!!");
-    }, 2000);
-  });
-}
